8316561897ea7f6940d5224c6e953d9bfc91326c
anascsk <84561602+anascsk@users.noreply.github.com>
2022-01-02 11:23:16 +0530

    initial

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+import {Container} from './styles/Navbar.style.js'
+
+
+const Navbar = () => {
+  return <Container>This is Navbar</Container>;
+};
+
+export default Navbar;
