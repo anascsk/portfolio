ae6e3256843c7b22a1b0d1ca8513a5a1ad29dd08
anascsk <84561602+anascsk@users.noreply.github.com>
2022-01-22 17:52:19 +0530

    project data update

--- src/assets/ProjectsData.js
+++ src/assets/ProjectsData.js
@@ -4,7 +4,7 @@ const projectsData = [
         title: 'audiobae.ml',
         body: 'An E-Commerce Web App which focused on selling audiophile products',
         image: 'https://res.cloudinary.com/anascsk/image/upload/v1642755683/anascoder%20portfolio/AudioBae-Rediscover-Music-at-India-s-1-Audiophile-Store_pfwpuw.png',
-        tech: `REACT  COMMERCE JS  STRIPE  FIREBASE  STYLED-COMPONENTS`,
+        tech: `REACT | COMMERCE JS | STRIPE | FIREBASE | STYLED-COMPONENTS`,
         linkgithub:'https://github.com/anascsk/audiobae',
         linkdemo: 'https://audiobae.ml'
         },
@@ -13,7 +13,7 @@ const projectsData = [
         title: 'AirBae.ml',
         body: "Clone of AirBnb.com. letting users to authenticate and reserve rooms by picking dates",
         image: 'https://res.cloudinary.com/anascsk/image/upload/v1642755687/anascoder%20portfolio/Airbae-Holiday-Rentals-Cabins-Beach-Houses-Unique-Homes-Experiences_w09tcx.png',
-        tech: `REACT JS  FIREBASE  STYLED-COMPONENTS  REACT-DATE-PICKER`,
+        tech: `REACT JS | FIREBASE | STYLED-COMPONENTS | REACT-DATE-PICKER`,
         linkgithub:'https://github.com/anascsk/Airbnb-clone',
         linkdemo: 'http://airbae.ml/'
         },
@@ -22,7 +22,7 @@ const projectsData = [
         title: 'WebDev Portfolio',
         body: "A minimalistic portfolio website using React js and UI styled with Styled-Components.",
         image: 'https://res.cloudinary.com/anascsk/image/upload/v1642755682/anascoder%20portfolio/Mohammed-Anas_lpxhur.png',
-        tech: `REACT JS  STYLED-COMPONENTS  REACT ROUTER  `,
+        tech: `REACT JS | STYLED-COMPONENTS | REACT-ROUTER | REACT-SCROLL `,
         linkgithub:'https://github.com/anascsk/portfolio',
         linkdemo: 'http://anascoder.ml/'
         },
@@ -31,7 +31,7 @@ const projectsData = [
         title: 'Simple Calculator',
         body: "A simple calculator using React js with clean and modern User Interface",
         image: 'https://res.cloudinary.com/anascsk/image/upload/v1642755682/anascoder%20portfolio/Calculator_yzv6ze.png',
-        tech: `REACT JS  CSS3  `,
+        tech: `REACT JS | CSS3  `,
         linkgithub:'https://github.com/anascsk/Calculator"',
         linkdemo: 'https://simplycalc.netlify.app/'
         },
@@ -40,7 +40,7 @@ const projectsData = [
         title: 'Movie Reviews',
         body: "A Movie review Website using TDMB API and Vanilla JavaScript with a beautiful UI/UX Design.",
         image: 'https://res.cloudinary.com/anascsk/image/upload/v1642758174/anascoder%20portfolio/Movie-Reviews_rkkb5g.png',
-        tech: `HTML  CSS  JAVASCRIPT  TDMB API`,
+        tech: `HTML | CSS | JAVASCRIPT | TDMB API`,
         linkgithub:'https://github.com/anascsk/movie-reviews',
         linkdemo: 'https://anascsk.github.io/movie-reviews/'
         },
--- src/styles/Navbar.style.js
+++ src/styles/Navbar.style.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 import { mobile, tablet } from "../responsive";
 
 export const Container = styled.div`
